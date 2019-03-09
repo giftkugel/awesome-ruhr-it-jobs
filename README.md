f73c67ec514a69b8687e60ddd4605ac73b02e5e8
giftkugel <github@giftkugel.de>
2019-03-09 16:09:13 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # awesome-ruhr-jobs
-A curated list  of employers for software developers
+A curated list of employers for software developers and technology freaks in the German Ruhr area. :blush:
+
+## Why?
+Before I joined the awesome [Karakun AG](https://dev.karakun.com), I was looking for possible employers in the German Ruhr area. I collected a bunch of links of nice companies. This list has become stale in my browser and many colleagues asked for it. Now I decided to create an awesome list to allow colleagues looking for a job do find one!
+
+## Contribute
+It's a [React](https://reactjs.org/) based application. All data is stored in `data.js`. If you want to add a company, just create a pull request on `data.js`.
+
+## Get a nice job!
+Have fun finding a good job! :smiley:
+
+Share this list on Twitter, or somewhere else,, so more persons can get the list and get the right job!
---
+++ docs/data.js
@@ -0,0 +1,314 @@
+const awesome = [
+    {
+        name: "Bilstein Group",
+        url: "https://www.bilsteingroup.com/karrierewelt/",
+        locations: ["Ennepetal"],
+        tags: ["java"]
+    },
+    {
+        name: "Ista",
+        url: "https://career.ista.com/de/",
+        locations: ["Dortmund", "Essen", "Gladbeck", "Münster"],
+    },
+    {
+        name: "Zalando",
+        url: "https://jobs.zalando.com/tech/jobs/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Euromicron",
+        url: "https://www.euromicron-karriere.de/",
+        locations: ["Essen", "Hamm"],
+    },
+    {
+        name: "Itemis",
+        url: "https://www.itemis.com/de/jobs/arbeiten-bei-itemis/",
+        locations: ["Essen", "Lünen"],
+    },
+    {
+        name: "Abbott",
+        url: "https://www.jobs.abbott/global/de",
+        locations: ["Witen"],
+    },
+    {
+        name: "Elmos",
+        url: "https://www.elmos.com/karriere/ausschreibungen/offene-stellen.html",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "KVWL",
+        url: "https://karriere-kvwl.de/startseite/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Cosinex",
+        url: "https://www.cosinex.de/unternehmen/karriere.html",
+        locations: ["Bochum"],
+    },
+    {
+        name: "BFS health finance",
+        url: "https://meinebfs.de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Remondis",
+        url: "https://www.remondis-karriere.de/stellenmarkt/stellenangebote/",
+        locations: ["Lünen"],
+    },
+    {
+        name: "GEA",
+        url: "https://www.gea.com/de/karriere/index.jsp",
+        locations: ["Bönen"], 
+    },
+    {
+        name: "Volkswagen Infotainment",
+        url: "https://www.volkswagen-infotainment.com/de/career.html",
+        locations: ["Bochum"], 
+    },
+    {
+        name: "Deutsche Edelstahlwerke Services",
+        url: "https://www.dew-stahl.com/karriere-bei-dew/jobboerse/",
+        locations: ["Witten"], 
+    },
+    {
+        name: "Manufactum",
+        url: "https://www.manufactum.de/aktuelle-stellenangebote-c199369/",
+        locations: ["Waltrop"], 
+    },
+    {
+        name: "OpusCapita",
+        url: "https://www.opuscapita.de/karriere/offene-positionen",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Nubizz",
+        url: "http://www.nubizz.de/karriere/index.html",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Netconomy",
+        url: "https://www.netconomy.net/de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Trilux",
+        url: "https://www.trilux.com/de/unternehmen/karriere/stellenangebote/",
+        locations: ["Arnsberg"],
+    },
+    {
+        name: "Grohe",
+        url: "https://grohe.softgarden.io/de/vacancies",
+        locations: ["Hemer"],
+    },
+    {
+        name: "Reknova",
+        url: "https://www.reknova.com/de/karriere.html",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Amprion",
+        url: "https://www.amprion.net/Karriere/Karriereportal/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Beumer Group",
+        url: "https://www.beumergroup.com/de/jobs-karriere/stellenangebote",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "DEW21",
+        url: "https://www.dew21.de/ueber-dew21/karriere/stellenmarkt/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "QuinScape",
+        url: "https://www.quinscape.de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Arithnea",
+        url: "https://www.arithnea.de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "H&D",
+        url: "https://www.hud-karriere.de/stellenangebote/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Open-Xchange",
+        url: "https://www.open-xchange.com/about-ox/career/jobs/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Schülerhilfe",
+        url: "https://www.schuelerhilfe.de/unternehmen/jobs/weitere-jobangebote/",
+        locations: ["Gelsenkirchen"],
+    },
+    {
+        name: "Rhenus Logistics",
+        url: "https://www.rhenus.com/de/de/jobboerse/",
+        locations: ["Holzwickede"],
+    },
+    {
+        name: "Land NRW",
+        url: "https://www.stellenmarkt.nrw.de/jobs/search/",
+        locations: ["Hagen"],
+    },
+    {
+        name: "Grandcentrix",
+        url: "https://grandcentrix.net/karriere/offene-stellen/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Agido",
+        url: "https://www.agido.com/jobs",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Ingenit",
+        url: "https://www.ingenit.com/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Tedi",
+        url: "https://www.tedi.com/job-karriere/stellenangebote/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Comcave College",
+        url: "https://comcave-karriere.de/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Ortis",
+        url: "https://www.otris.de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "MotionMiners",
+        url: "https://www.motionminers.com/jobs.html",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Vorwerk",
+        url: "https://www.career-vorwerkgroups.com/job-offers.html",
+        locations: ["Wuppertal"],
+    },
+    {
+        name: "Reply",
+        url: "https://www.reply.com/de/about/careers/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Uniq",
+        url: "https://www.un-iq.de/de/jobs/",
+        locations: ["Holzwickede"],
+    },
+    {
+        name: "Osudio",
+        url: "https://jobs.osudio.com/",
+        locations: ["Lünen"],
+    },
+    {
+        name: "Raith",
+        url: "https://www.raith.com/company/career.html",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Adesso AG",
+        url: "https://www.adesso.de/de/jobs-karriere/unsere-stellenangebote/",
+        locations: ["Dortmund", "Essen"],
+    },
+    {
+        name: "Adesso Insurance Solutions",
+        url: "https://www.adesso-insure.de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Limbiq",
+        url: "https://www.limbiq.com/",
+        locations: ["Bochum"],
+    },
+    {
+        name: "Sonepar",
+        url: "https://www.sonepar.de/karriere/stellenangebote/",
+        locations: ["Holzwickede"],
+    },
+    {
+        name: "DSGF - Deutsche Servicegesellschaft für Finazdienstleister",
+        url: "https://www.dsgf.de/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Dogado",
+        url : "https://dogado.group/#karriere",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Maximago",
+        url: "https://www.maximago.de/jobs/",
+        locations: ["Lünen"],
+    },
+    {
+        name: "Communicode",
+        url: "https://www.communicode.de/karriere_bei_der_fullservice-digitalagentur_communicode-ag",
+        locations: ["Essen"],
+    },
+    {
+        name: "QVC",
+        url: "https://karriere.qvc.de/jobboerse/positionen-nach-standorten/",
+        locations: ["Bochum"],
+    },
+    {
+        name: "Tyntec",
+        url: "https://www.tyntec.com/jobs-tyntec",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "ICA Traffic",
+        url: "http://www.ica.de/traffic/unternehmen/karriere/",
+        locations: ["Dortmund"],
+    },
+    {
+        name: "Alten",
+        url: "https://de.alten.com/karriere/jobs.html",
+        locations: ["Essen", "Bochum", "Dortmund"],
+    },
+    {
+        name: "Fielddata.io GmbH",
+        url: "https://www.fielddata.io/de/jobs",
+        locations: ["Essen"],
+    },
+    {
+        name: "Escrypt",
+        url: "https://www.escrypt.com/de/Stellenangebote",
+        locations: ["Bochum"],
+    },
+    {
+        name: "Active Logistics",
+        url: "https://www.active-logistics.com/jobs/",
+        locations: ["Herdecke"],
+    },
+    {
+        name: "9elements",
+        url: "https://9elements.com/jobs",
+        locations: ["Bochum"],
+    },
+    {
+        name: "GDATA",
+        url: "https://www.gdata.de/ueber-g-data/karriere",
+        locations: ["Bochum"],
+    },
+    {
+        name: "Codecentric",
+        url: "https://www.codecentric.de/karriere/offene-stellen/",
+        locations: ["Dortmund", "Münster"],
+    },
+    {
+        name: "Smart Mechatronics",
+        url: "https://jobs.smartmechatronics.de/",
+        locations: ["Dortmund"],
+    }
+
+];
